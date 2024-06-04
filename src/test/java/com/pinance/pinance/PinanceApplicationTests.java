87d9f0ee504c672f09f9a93a1a03aaab424559ee
adamtlee <adam.lee346@gmail.com>
2024-06-03 19:17:34 -0600

    Initial commit

---
+++ src/test/java/com/pinance/pinance/PinanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pinance.pinance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PinanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
